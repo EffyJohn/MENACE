9cc34d1600e7724c11074e51a17b71d42dcbbe95
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-27 18:26:34 +0530

    added reflectVertical in transform.cc

--- include/transform.h
+++ include/transform.h
@@ -2,9 +2,11 @@
 #define TRANSFORM_H
 
 #include <bitset>
+#include <iostream>
 #define SIZE 9 
 
-std::bitset<SIZE> rotateRight(std::bitset<SIZE> input, int rotate_by);
-std::bitset<SIZE> rotate_90(std::bitset<SIZE> board);
+std::bitset<SIZE> bitRotateRight(std::bitset<SIZE> input, int rotate_by);
+std::bitset<SIZE> rotate90(std::bitset<SIZE> board);
+std::bitset<SIZE> reflectVertical(std::bitset<SIZE> board);
 
 #endif
--- src/transform.cc
+++ src/transform.cc
@@ -1,17 +1,15 @@
 #include "../include/transform.h"
 
 // Custom implementation of bit rotate right
-// !USES #DEFINED SIZE
 // @param: input: bitset to rotate
 // @param: rotate_by: number of places to rotate by
-std::bitset<SIZE> rotateRight(std::bitset<SIZE> input, int rotate_by){
+std::bitset<SIZE> bitRotateRight(std::bitset<SIZE> input, int rotate_by){
     return (input >> rotate_by | input << (SIZE - rotate_by));
 }
 
-// Function that takes a board and rotates it by 90 degrees ccw
-// !USES #DEFINED SIZE
+// Function that takes a board and returns a board that is rotated by 90 degrees ccw
 // @param: board: input board to rotate
-std::bitset<SIZE> rotate_90(std::bitset<SIZE> board){
+std::bitset<SIZE> rotate90(std::bitset<SIZE> board){
     // Initialise variables   
     std::bitset<SIZE> rotated_board(0);
     std::bitset<SIZE> current(0);
@@ -29,20 +27,43 @@ std::bitset<SIZE> rotate_90(std::bitset<SIZE> board){
         current = board&mask;
 
         // ! Board representation is 123456789, but bitset uses 987654231
-        current = rotateRight(current, encoder[SIZE - i - 1]);
+        current = bitRotateRight(current, encoder[SIZE - i - 1]);
         rotated_board |= current;
     }
     
     return rotated_board;
 }
 
-// Temporary main function
-int main(){
+// Function that takes a board and returns a board that is reflected about the vertical axis
+// @param: board: input board to reflect 
+std::bitset<SIZE> reflectVertical(std::bitset<SIZE> board){
+    // Initialize variables   
+    std::bitset<SIZE> reflected_board(0);
+    std::bitset<SIZE> current(0);
+    std::bitset<SIZE> mask(0);
    
+    // Encoder[i] = number of places to bitrotate i'th place on board
+    int encoder[SIZE] = {2, 0, 7, 2, 0, 7, 2, 0, 7};
+
+    for(int i = 0; i < SIZE; ++i){
+        // Mask to obtain i'th place on board
+        mask.reset();
+        mask.set(i);
+
+        // Obtain i'th place on board
+        current = board&mask;
+
+        // ! Board representation is 123456789, but bitset uses 987654231
+        current = bitRotateRight(current, encoder[SIZE - i - 1]);
+        reflected_board |= current;
+    }
+    return reflected_board;
+}
+
+// Temporary main function
+int main(){   
     // Sample board
     std::bitset<SIZE> board("011100001");
-    // Rotate by 90 degrees ccw
-    board = rotate_90(board);
-
+    board = reflectVertical(board);
     return 0;
 }
