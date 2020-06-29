d072f9bb9eb8ab7fe6d7f8dbe44105058d3e0fe4
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-29 13:44:36 +0530

    added Data class for database handling, uses unordered map

---
+++ include/data.h
@@ -0,0 +1,34 @@
+#ifndef DATA_H
+#define DATA_H
+
+
+#include <unordered_map>
+#include <math.h>
+#include <random>
+#include "param.h"
+
+typedef std::vector<uint32_t> MatchBoxType;
+typedef std::unordered_map<BoardKeyType, MatchBoxType> DatabaseType;
+
+class Data{
+    public:
+
+        Data(std::mt19937 *gen){
+            this->gen = gen;
+        }
+
+        BoardType getMove(BoardKeyType key);
+        void updateEntry(BoardKeyType key);         // TODO: Implement
+
+    private:
+
+        std::mt19937 *gen;
+        DatabaseType database;
+        
+        BoardType choosePlace(DatabaseType::iterator);
+        DatabaseType::iterator createEntry(BoardKeyType key);
+        
+};
+
+
+#endif
---
+++ src/data.cc
@@ -0,0 +1,56 @@
+#include "../include/data.h"
+
+// Function that returns a board of a randomly selected move from the database entry
+// @param: data_iter: iterator to required database entry (ie- board)
+BoardType Data::choosePlace(DatabaseType::iterator data_iter){
+ 
+    // Create a cdf from the number of beads for each place
+    std::discrete_distribution<uint32_t> dist(data_iter->second.begin(), data_iter->second.end());
+ 
+    // Choose a place from the cdf
+    // returns index ie- 0 to 8
+    uint32_t place = dist(*(this->gen));
+
+    
+    BoardType move("100000000");
+    return (move >> place);
+}
+
+// Function that creates a database entry for the key
+// @param: key for the current board
+DatabaseType::iterator Data::createEntry(BoardKeyType key){
+ 
+    // Create entry with all 0's
+    MatchBoxType data_entry(SIZE, 0);
+
+    // Calculate vacant positions available
+    // TODO: Eliminate symmetry cases
+    BoardType occupied = key;          // Stores lower 9 bits into occupied
+    occupied &= (key/pow(2, SIZE));    // Bitwise ands next 9 bits from key into occupied
+
+    // If bit not set, add beads for that position
+    for(int i = 0; i < SIZE; ++i){
+        if (!occupied[i])
+            data_entry[SIZE - 1 - i] = 5;   // Bitset indexes R to L
+            // TODO: Number of initial beads
+    }
+
+    std::pair<DatabaseType::iterator, bool> stored = database.insert(make_pair(key, data_entry));
+    return stored.first;
+
+}
+
+// Function that takes the database key and returns the move
+// @param: key: key for the current board 
+BoardType Data::getMove(BoardKeyType key){
+    
+    DatabaseType::iterator data_iter;
+    data_iter = database.find(key);
+
+    // If board not in database, create
+    if (data_iter == database.end())
+        data_iter = createEntry(key);
+    
+    return choosePlace(data_iter);
+
+}
