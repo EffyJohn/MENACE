5455d60db67f514fe228879967977b8a1c895f61
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-29 14:27:39 +0530

    added basic main.cc and moved main function out of transform.cc

---
+++ src/main.cc
@@ -0,0 +1,17 @@
+#include "../include/transform.h"
+#include "../include/data.h"
+#include "../include/param.h"
+
+
+int main(){   
+    // Sample board
+    BoardType noughts("011100001");
+    BoardType crosses("100000100");
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    Data d = Data(&gen);
+    Transform t = Transform(&d);
+    Transform::QueryResult q = t.makeMove(noughts, crosses);
+    std::cout << q.transformation << std::endl;
+    return 0;
+}
--- src/transform.cc
+++ src/transform.cc
@@ -205,16 +205,3 @@ Transform::QueryResult Transform::makeMove(BoardType noughts, BoardType crosses)
 }
 
 
-// Temporary main function
-int main(){   
-    // Sample board
-    BoardType noughts("011100001");
-    BoardType crosses("100000100");
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    Data d = Data(&gen);
-    Transform t = Transform(&d);
-    Transform::QueryResult q = t.makeMove(noughts, crosses);
-    std::cout << q.transformation << std::endl;
-    return 0;
-}
